36af2c226beab37ddf2126bf2060a08d9a744690
Johan Smit <johansm@bbd.co.za>
2025-08-31 17:40:23 +0200

    Almost done

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,11 @@
+const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+const firstDate = Date.now();
+const secondDate = new Date(2026, 4, 4);
+
+const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
+
+document.getElementById("days-to-go").innerHTML = `${diffDays} dae om te gaan!`;
+
 let navOpen = false;
 
 function toggleNav() {
@@ -21,9 +29,15 @@ function toggleNav() {
 }
 
 
+const rsvpInputs = document.getElementById("rsvp-inputs");
 const rsvpAmountSelect = document.getElementById("rsvp-total-select");
 const rsvpNamesContainer = document.getElementById("rsvp-names-container");
 const form = document.getElementById("rsvp-form");
+const additionalNotesSection = document.getElementById("additional-notes");
+additionalNotesSection.style.display = "none";
+
+const submitButton = document.getElementById("rsvp-form-submit-button");
+submitButton.style.visibility = "hidden";
 
 // Generate guest fields when number is selected
 rsvpAmountSelect.addEventListener("change", () => {
@@ -31,12 +45,16 @@ rsvpAmountSelect.addEventListener("change", () => {
   const count = parseInt(rsvpAmountSelect.value, 10);
 
   if (!isNaN(count)) {
+
+    additionalNotesSection.style.display = "flex";
+    submitButton.style.visibility = "visible";
+
     for (let i = 1; i <= count; i++) {
       const guestContainer = document.createElement("section");
       guestContainer.id = `guest_${i}`;
 
       const nameLabel = document.createElement("label");
-      nameLabel.textContent = `Gas ${i} se naam: `;
+      nameLabel.textContent = `Naam van gas ${i}: `;
       const nameInput = document.createElement("input");
       nameInput.type = "text";
       nameInput.name = `guest_name_${i}`;
@@ -50,8 +68,8 @@ rsvpAmountSelect.addEventListener("change", () => {
 
       guestContainer.appendChild(nameLabel);
       guestContainer.appendChild(nameInput);
-      guestContainer.appendChild(attendanceLabel);
-      guestContainer.appendChild(attendanceCheckbox);
+      // guestContainer.appendChild(attendanceLabel);
+      // guestContainer.appendChild(attendanceCheckbox);
 
       rsvpNamesContainer.appendChild(guestContainer);
     }
@@ -61,8 +79,12 @@ rsvpAmountSelect.addEventListener("change", () => {
     const additionalNotesTextArea = document.createElement("textarea");
     additionalNotesTextArea.name = "notes";
 
-    rsvpNamesContainer.appendChild(additionaNotesLabel);
-    rsvpNamesContainer.appendChild(additionalNotesTextArea);
+    // rsvpNamesContainer.appendChild(additionaNotesLabel);
+    // rsvpNamesContainer.appendChild(additionalNotesTextArea);
+  }
+  else{
+    additionalNotesSection.style.display = "none";
+    submitButton.style.visibility = "hidden";
   }
 });
 
@@ -75,9 +97,10 @@ form.addEventListener("submit", async (e) => {
 
   for (let i = 1; i <= count; i++) {
     const name = document.querySelector(`#guest_${i} input[type="text"]`).value;
-    const attending = document.querySelector(
-      `#guest_${i} input[type="checkbox"]`
-    ).checked;
+    // const attending = document.querySelector(
+    //   `#guest_${i} input[type="checkbox"]`
+    // ).checked;
+    const attending = true;
     guests.push({ name, attending });
   }
 
@@ -90,5 +113,68 @@ form.addEventListener("submit", async (e) => {
   });
 
   const result = await response.json();
+
+  if (result.success) {
+    rsvpInputs.innerHTML = "";
+
+    const completedSection = document.createElement("section");
+    completedSection.id = "completedSection";
+
+    const completedMessage = document.createElement("p");
+    completedMessage.innerHTML = "RSVP is gestuur. Ons kan nie wag om ons spesiale dag met julle te spandeer nie!";
+
+    const cheersContainer = document.createElement("section");
+    cheersContainer.id = "cheers-container";
+
+    const cheersGif = document.createElement("img");
+    cheersGif.src = "./img/cheers-2.gif"
+
+    cheersContainer.appendChild(cheersGif);
+
+    completedSection.appendChild(completedMessage);
+    completedSection.appendChild(cheersContainer);
+    rsvpInputs.appendChild(completedSection);
+
+    document.cookie = "completed=true; expires=Sun, 5 Apr 2026 12:00:00 UTC";
+  }
+
   alert(result.message);
 });
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+if (getCookie("completed") == "true") {
+    rsvpInputs.innerHTML = "";
+
+    const completedSection = document.createElement("section");
+    completedSection.id = "completedSection";
+
+    const completedMessage = document.createElement("p");
+    completedMessage.innerHTML = "RSVP is gestuur Ons kan nie wag om ons dag met julle te spandeer nie!";
+
+    const cheersContainer = document.createElement("section");
+    cheersContainer.id = "cheers-container";
+
+    const cheersGif = document.createElement("img");
+    cheersGif.src = "./img/cheers-2.gif"
+
+    cheersContainer.appendChild(cheersGif);
+
+    completedSection.appendChild(completedMessage);
+    completedSection.appendChild(cheersContainer);
+    rsvpInputs.appendChild(completedSection);
+}
